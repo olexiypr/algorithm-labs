dcb26df1ca16c14f85997623737ac8c0990c8b91
olexiypr <olexiypr>
2022-10-14 11:12:37 +0300

    Update

--- Laba2/Laba2/Program.cs
+++ Laba2/Laba2/Program.cs
@@ -8,27 +8,23 @@ namespace Laba2
         public static void Main(string[] args)
         {
             var random = new Random();
-            for (int i = 0; i < 15; i++)
+            var grid = new Grid();
+            var startStr = random.Next(2);
+            for (int j = 0; j < startStr; j++)
             {
-                var grid = new Grid();
-                var startStr = random.Next(0);
-                startStr = 3;
-                for (int j = 0; j < startStr; j++)
-                {
-                    grid[random.Next(8), j].IsEmpty = false;
-                }
-                Console.WriteLine("Input chessboard: ");
-                grid.Print();
-                var bfs = new BFS(grid);
-                var res = bfs.Start(startStr);
-                res = bfs.IsValid();
-                Console.WriteLine("Success: " + res);
-                Console.WriteLine("Result chessboard");
-                grid.Print();
-                Console.WriteLine("Count iterations: " + bfs.iterationCount);
-                Console.WriteLine("Dead ends count: " + bfs.deadEndCount);
-                Console.WriteLine("States count: " + bfs.statesCount);
+                grid[random.Next(8), j].IsEmpty = false;
             }
+            Console.WriteLine("Input chessboard: ");
+            grid.Print();
+            var bfs = new BFS(grid);
+            var res = bfs.Start(startStr);
+            res = bfs.IsValid();
+            Console.WriteLine("Success: " + res);
+            Console.WriteLine("Result chessboard");
+            grid.Print();
+            Console.WriteLine("Count iterations: " + bfs.iterationCount);
+            Console.WriteLine("Dead ends count: " + bfs.deadEndCount);
+            Console.WriteLine("States count: " + bfs.statesCount);
         }
     }
 }
